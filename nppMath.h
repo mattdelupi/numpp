0f3852d119009219f2021211f0feea8c4105c72c
mattdelupi <matt.delupi@gmail.com>
2025-08-28 18:52:35 +0200

    added checks for division by zero for the Vctr class

--- nppMath.h
+++ nppMath.h
@@ -7,7 +7,7 @@ namespace npp
   template <class T>
   void checkDivision(const T &value)
   {
-    if (value == T{})
+    if (value == 0.0)
     throw (std::runtime_error("division by zero occurred"));
   }
 }
